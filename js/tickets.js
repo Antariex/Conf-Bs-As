985569a4986b7923c16baa0f384c1a0b414c2474
Ariel <arielantequiera@gmail.com>
2022-11-07 19:08:20 -0300

    se arreglan tarjetas mobile

--- js/tickets.js
+++ js/tickets.js
@@ -6,6 +6,4 @@ resumir = () => {
     let precio = cantidad * (precioTicket - ((precioTicket * descuento)/200));
 
     document.getElementById('compraTotal') = 'Total a pagar: $' + precio;
-
-    console.log(precio)
 }
