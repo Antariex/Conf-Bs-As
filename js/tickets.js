a78c73434a64de4b60d3ce0a2024504ae2665777
Ariel <arielantequiera@gmail.com>
2022-11-07 18:42:23 -0300

    se agregan atributos

--- js/tickets.js
+++ js/tickets.js
@@ -1,9 +1,11 @@
 const precioTicket = 200;
 
 resumir = () => {
-    let cantidad = document.getElementById('cantidad').value;
-    let descuento = document.getElementById('categoria').value;
+    let descuento = document.getElementById('categoria');
+    let cantidad = document.getElementById('cantidad');
     let precio = cantidad * (precioTicket - ((precioTicket * descuento)/200));
 
-    document.getElementById('compraTotal').value = 'Total a pagar: $' + precio;
+    document.getElementById('compraTotal') = 'Total a pagar: $' + precio;
+
+    console.log(precio)
 }
