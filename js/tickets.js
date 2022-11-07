3db48936ab5373297c4a2cb44afc8c13c597f98d
Ariel <arielantequiera@gmail.com>
2022-11-07 19:42:28 -0300

    Se corrije función

--- js/tickets.js
+++ js/tickets.js
@@ -3,7 +3,7 @@ const precioTicket = 200;
 resumir = () => {
     let cantidad = document.getElementById('cantidad').value;
     let descuento = document.getElementById('categoria').value;
-    let precio = cantidad * (precioTicket - ((precioTicket * descuento)/200));
+    let precio = cantidad * (precioTicket - ((precioTicket * descuento)/100));
 
     document.getElementById('compraTotal').value = precio;
 }
